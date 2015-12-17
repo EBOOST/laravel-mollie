493051d2d66412730f338ddb58fa75dd3ecdad88
bertvanhoekelen <bert@eboost.co>
2015-12-17 16:20:46 +0100

    Added merge config so you don't have to publish the config.

--- src/LaravelMollieServiceProvider.php
+++ src/LaravelMollieServiceProvider.php
@@ -7,14 +7,27 @@ use Illuminate\Support\ServiceProvider;
 class LaravelMollieServiceProvider extends ServiceProvider
 {
     /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/mollie.php' => config_path('mollie.php'),
+        ], 'config');
+    }
+
+    /**
      * Register any package services.
      *
      * @return void
      */
     public function register()
     {
-        $configPath = __DIR__ . '/../config/mollie.php';
-        $this->publishes([$configPath => config_path('mollie.php')]);
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/mollie.php', 'mollie'
+        );
 
         $this->app['mollie'] = $this->app->share(function ($app) {
             $config = $app['config']->get('mollie');
